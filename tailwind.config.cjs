934ae89a738c66e91dab2176a8e0a8a6b2eca778
George Mwaniki <mwaniki.george@gmail.com>
2022-08-13 12:39:25 +0300

    Added loading screen and animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,39 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'float': 'float 3s ease-in-out infinite',
+        'slide-in': 'slide-in 0.5s ease-in-out forwards',
+      },
+      keyframes: {
+        'float': {
+          '0%': {
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            transform: 'translateY(60px)',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+          },
+      },
+      'slide-in': {
+        '0%': {
+          transform: 'translateY(50px)',
+          opacity: 0,
+        },
+        '100%': {
+          transform: 'translateY(0)',
+          opacity: 1,
+        },
+        
+    },
   },
   plugins: [],
+}
+}
 }
